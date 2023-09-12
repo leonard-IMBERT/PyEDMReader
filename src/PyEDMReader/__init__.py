93025389151c2ca0938765e02ea9cf0848661702
Leonard Imbert <leonard.imbert1@etu.uni-nantes.fr>
2023-09-12 13:13:57 +0200

    [Janne] Fix Janne support

--- src/PyEDMReader/__init__.py
+++ src/PyEDMReader/__init__.py
@@ -5,12 +5,19 @@ for path in __path__:
     ctypes.cdll.LoadLibrary(os.path.join(path, "libEDMReader.so"))
 
 from .PyEDMReader import EDMReader, Event, Hit, Truth, __doc__, __version__
+from .JaEDMReader import JaEDMReader, JaEDMReaderConfig
+
+janne = {
+        "JaEDMReader": JaEDMReader,
+        "JaEDMReaderConfig": JaEDMReaderConfig
+        }
 
 __all__ = [
         "EDMReader",
         "Event",
         "Hit",
         "Truth",
+        "janne",
         "__doc__",
         "__version__"
         ]
