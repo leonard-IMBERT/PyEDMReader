5834b994326ab4cfc222c58a85932dacf8f57931
Leonard Imbert <leonard.imbert1@etu.uni-nantes.fr>
2023-09-29 16:53:30 +0200

    [Fix] Fix the __init__

--- src/PyEDMReader/__init__.py
+++ src/PyEDMReader/__init__.py
@@ -5,7 +5,7 @@ for path in __path__:
     _ctypes.cdll.LoadLibrary(_os.path.join(path, "libEDMReader.so"))
 
 from .PyEDMReader import EDMReader, Event, Hit, Truth, __doc__, __version__
-from _jaedmreader import JaEDMReader, JaEDMReaderConfig
+from ._jaedmreader import JaEDMReader, JaEDMReaderConfig
 
 
 __all__ = [
